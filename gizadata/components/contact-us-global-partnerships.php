afc3c976c283ef5dca3dd8f61072b82f288e124c
Cong Le <congledev99@gmail.com>
2025-07-12 10:16:05 +0700

    feat: add session contact us global partnerships

---
+++ gizadata/components/contact-us-global-partnerships.php
@@ -0,0 +1,47 @@
+<section class="contact-us-global-partnerships session-wrap relative">
+    <div class="container contact-us-container-custom">
+        <div class="row">
+            <div class="col-12 mb-3">
+                <h2 class="title">HỢP TÁC QUỐC TẾ | GIA TĂNG NGUỒN LỰC</h2>
+            </div>
+        </div>
+        <div class="row gy-2 md-gy-4">
+            <!-- Cột A: 6 logo đối tác -->
+            <div class="col-12 col-md-6">
+                <div class="partnerships-item d-flex flex-column align-items-center justify-content-center">
+                    <div class="partnerships-item-title mb-3">
+                        <h3>Hợp tác với các viện thành viên HĐQT trong ASEAN</h3>
+                    </div>
+                    <div class="row w-100 g-2">
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-1.png" alt="partnership-1"></div>
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-2.png" alt="partnership-2"></div>
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-3.png" alt="partnership-3"></div>
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-4.png" alt="partnership-4"></div>
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-5.png" alt="partnership-5"></div>
+                        <div class="col-6 col-md-4"><img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/images/partnership-6.png" alt="partnership-6"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- Cột B: 1 hình, box ACGN, content -->
+            <div class="col-12 col-md-6">
+                <div class="partnerships-item d-flex flex-column align-items-center justify-content-center">
+                    <div class="partnerships-item-title mb-3">
+                        <h3>Thành viên các mạng lưới</h3>
+                    </div>
+                    <div class="mb-3 w-100 d-flex justify-content-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/parnership-0.png" alt="parnership-0" class="img-fluid" style="max-width:220px;">
+                    </div>
+                    <div class="partnerships-item-content mb-3 d-block d-md-none">
+                        <p class="viod-member-content text-center mb-0">VIOD là thành viên của Mạng lưới Viện Thành viên Hội đồng<br>Quản trị toàn cầu (GNDI)</p>
+                    </div>
+                    <div class="acgn-box w-100 d-flex align-items-center justify-content-center mt-2">
+                        <span class="acgn-text">ASEAN<br>CLIMATE GOVERNANCE NETWORK<br>(ACGN)</span>
+                    </div>
+                    <div class="partnerships-item-content mb-3 mt-3 d-block d-md-none">
+                        <p class="viod-member-content text-center mb-0">VIOD là thành viên của Mạng lưới ASEAN về Quản trị Khí hậu</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
