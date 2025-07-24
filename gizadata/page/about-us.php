ddf0ce4d975237ed620b38a2d1befe8a4205d2cd
Cong Le <congledev99@gmail.com>
2025-07-24 22:08:49 +0700

    fix: fix css

--- gizadata/page/about-us.php
+++ gizadata/page/about-us.php
@@ -44,10 +44,10 @@ get_header();
                 </div>
             </div>
             <div class="col-12 col-lg-6">
-                <div class="content-text">
+                <div class="content-text >
                     <!-- <h2 class="title desktop-title d-none d-lg-block">TỔNG QUAN VỀ CHƯƠNG TRÌNH</h2> -->
                     <div class="description">
-                        <div class="text-content" id="textContent">
+                        <div class="text-content text-content-about-us"" id="textContent">
                             <p>Viện Thành viên Hội đồng Quản trị Việt Nam (VIOD) là định chế chuyên nghiệp, độc lập và tiên phong về Quản trị Công ty (QTCT) và Phát triển bền vững tại Việt Nam. VIOD có sứ mệnh thúc đẩy nâng cao chất lượng và mặt bằng QTCT thông qua đào tạo, huấn luyện và đánh giá thực hành QTCT theo luật định, các chuẩn mực và thông lệ quốc tế. Các hoạt động của VIOD hướng đến chuyên nghiệp hóa Hội đồng Quản trị (HĐQT), hỗ trợ thúc đẩy các chuẩn mực cao về tính minh bạch và đạo đức kinh doanh. VIOD đồng thời xây dựng và phát triển mạng lưới những người thực hành và làm Nghề QTCT, đó là các thành viên HĐQT, các thành viên phụ trách & thư ký QTCT, các nhà tư vấn đến từ các công ty tư vấn trong hệ sinh thái QTCT của thị trường.</p>
                             <!-- <p class="extended-content">Chương trình DCP cung cấp các kiến thức về QTCT bao gồm vai trò, trách nhiệm của các HĐQT, chức năng của các Ủy ban chuyên trách, hướng đến chuyên nghiệp hóa QTCT tại Việt Nam, phù hợp với sự phát triển của thị trường tài chính trong nước, trong khu vực ASEAN và trên thế giới. DCP được công nhận rộng rãi cả về chất lượng học thuật, tính thực tiễn trong các tình huống nghiên cứu và chất lượng chuyên môn của đội ngũ người hướng dẫn, các khách mời chuyên gia trong phiên thảo luận.</p> -->
                         </div>
