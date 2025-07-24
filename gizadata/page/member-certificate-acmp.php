aca415c7de104e2da772acdd1efa45f3ce5b9662
Cong Le <congledev99@gmail.com>
2025-07-24 22:28:52 +0700

    fix: css

--- gizadata/page/member-certificate-acmp.php
+++ gizadata/page/member-certificate-acmp.php
@@ -8,7 +8,7 @@ get_header();
     <div class="banner banner-acmp">
         <!-- Breadcrumb chỉ hiện trên desktop -->
         <nav class="breadcrumb-nav d-none d-md-block">
-            <ol class="breadcrumb">
+            <ol class="breadcrumb dark">
                 <li class="breadcrumb-item"><a href="<?php echo home_url(); ?>">Home</a></li>
                 <li class="breadcrumb-item"><a href="<?php echo get_post_type_archive_link('training_program'); ?>">Chương trình đào tạo</a></li>
                 <li class="breadcrumb-item active">Audit Committee Master Program (ACMP)</li>
